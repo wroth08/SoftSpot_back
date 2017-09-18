f88b9fc4d12553723b582fda63f62f72b112cef3
WESTON ROTH <weston.roth08@gmail.com>
2017-09-18 13:50:42 -0600

    added ratings

--- routes/tech-route.js
+++ routes/tech-route.js
@@ -3,15 +3,16 @@ var knex = require('../knex')
 var router = express.Router()
 
 router.get('/', function (req, res) {
-  knex.select('name', 'description', 'url', 'category')
+  knex.select('name', 'description', 'url', 'category', 'rating')
     .from('tech')
     .then( function (data) {
       res.json(data)
     })
 })
+
 router.get('/:id', function (req, res) {
   let id = req.params.id
-  knex.select('name', 'description', 'url', 'category')
+  knex.select('name', 'description', 'url', 'category', 'rating')
     .from('tech')
     .where('id', id)
     .then( function (data) {
