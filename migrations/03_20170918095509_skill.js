30b4e7cd129478bf184b10da485c8215b89b0966
Zachariah Lowe <lowezake@gmail.com>
2017-09-18 14:03:47 -0600

    updated profession

--- migrations/03_20170918095509_skill.js
+++ migrations/03_20170918095509_skill.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('skill', (table) => {
   table.increments('id');
   table.text('name');
+  table.boolean('is_professional');
 });
 };
 
