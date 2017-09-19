59cec4ff2b329a04a561cbb999850d3e162806f2
WESTON ROTH <weston.roth08@gmail.com>
2017-09-19 10:01:58 -0600

    probably doesn't work but endorse post route

--- routes/endorse-route.js
+++ routes/endorse-route.js
@@ -2,4 +2,14 @@ var express = require('express')
 var knex = require('../knex')
 var router = express.Router()
 
+router.post('/', function (req, res) {
+  var row = req.body
+  knex('endorse')
+    .insert(row)
+    .returning('*')
+      .then( () => {
+        res.json(row);
+      })
+})
+
 module.exports = router
